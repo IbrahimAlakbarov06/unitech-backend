833067c5c07d7da4ddabe6015bf33eaa7f18d8a1
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-02 15:30:54 +0400

    api-gateway JwtService and Dockerfile attached

---
+++ api-gateway/src/main/java/org/unitech/apigateway/util/JwtService.java
@@ -0,0 +1,50 @@
+package org.unitech.apigateway.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class JwtService {
+
+    @Value("${jwt.secret:mySecretKey}")
+    private String secret;
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public Long extractExpiration(String token) {
+        return extractClaims(token).get("userId", Long.class);
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
