3fac2646b2b7d8c50d479de45ed8fe5811a26fb0
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-07 13:59:22 +0400

    ms-auth

--- ms-auth/src/main/java/org/unitech/msauth/config/RabbitMQConfig.java
+++ ms-auth/src/main/java/org/unitech/msauth/config/RabbitMQConfig.java
@@ -1,78 +1,51 @@
 package org.unitech.msauth.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMQConfig {
 
-    public static final String USER_EXCHANGE = "user.exchange";
-    public static final String USER_CREATED_QUEUE = "user.created.queue";
-    public static final String USER_UPDATED_QUEUE = "user.updated.queue";
-    public static final String USER_DELETED_QUEUE = "user.deleted.queue";
-
-    public static final String USER_CREATED_ROUTING_KEY = "user.created";
-    public static final String USER_UPDATED_ROUTING_KEY = "user.updated";
-    public static final String USER_DELETED_ROUTING_KEY = "user.deleted";
-
     @Bean
-    public TopicExchange userExchange() {
-        return new TopicExchange(USER_EXCHANGE);
+    public Queue userRegistrationQueue() {
+        return new Queue("user.registration", true);
     }
 
     @Bean
-    public Queue userCreatedQueue() {
-        return QueueBuilder.durable(USER_CREATED_QUEUE).build();
+    public Queue userLoginQueue() {
+        return new Queue("user.login", true);
     }
 
     @Bean
-    public Queue userUpdatedQueue() {
-        return QueueBuilder.durable(USER_UPDATED_QUEUE).build();
+    public Queue userLogoutQueue() {
+        return new Queue("user.logout", true);
     }
 
     @Bean
-    public Queue userDeletedQueue() {
-        return QueueBuilder.durable(USER_DELETED_QUEUE).build();
+    public Queue userUpdateQueue() {
+        return new Queue("user.update", true);
     }
 
     @Bean
-    public Binding userCreatedBinding() {
-        return BindingBuilder
-                .bind(userCreatedQueue())
-                .to(userExchange())
-                .with(USER_CREATED_ROUTING_KEY);
+    public Queue userDeleteQueue() {
+        return new Queue("user.delete", true);
     }
 
     @Bean
-    public Binding userUpdatedBinding() {
-        return BindingBuilder
-                .bind(userUpdatedQueue())
-                .to(userExchange())
-                .with(USER_UPDATED_ROUTING_KEY);
-    }
-
-    @Bean
-    public Binding userDeletedBinding() {
-        return BindingBuilder
-                .bind(userDeletedQueue())
-                .to(userExchange())
-                .with(USER_DELETED_ROUTING_KEY);
-    }
-
-    @Bean
-    public MessageConverter jsonMessageConverter() {
+    public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(jsonMessageConverter());
+        template.setMessageConverter(messageConverter());
         return template;
     }
 }
