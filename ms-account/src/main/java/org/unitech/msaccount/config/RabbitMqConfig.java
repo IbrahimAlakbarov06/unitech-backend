3fcc678780824ba34bf37a5b10095a9677e21446
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-08 21:20:09 +0400

    RabbitMqConfig completed in ms-account

---
+++ ms-account/src/main/java/org/unitech/msaccount/config/RabbitMqConfig.java
@@ -0,0 +1,41 @@
+package org.unitech.msaccount.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMqConfig {
+
+    @Bean
+    public Queue accountCreatedQueue() {
+        return new Queue("account.created", true);
+    }
+
+    @Bean
+    public Queue accountBlockedQueue() {
+        return new Queue("account.blocked", true);
+    }
+
+    @Bean
+    public Queue pinUpdatedQueue() {
+        return new Queue("account.pin.updated", true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
