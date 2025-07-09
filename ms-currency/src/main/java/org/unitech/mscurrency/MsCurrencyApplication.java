f21517353aa2f84dae1f76a60c93b92f90a62e3f
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-09 18:43:08 +0400

    ms-currency created

---
+++ ms-currency/src/main/java/org/unitech/mscurrency/MsCurrencyApplication.java
@@ -0,0 +1,15 @@
+package org.unitech.mscurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCurrencyApplication.class, args);
+    }
+
+}
---
+++ ms-currency/src/test/java/org/unitech/mscurrency/MsCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unitech.mscurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
