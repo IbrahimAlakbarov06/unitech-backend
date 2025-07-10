647a1e3809aca9071998d632c05bc520e9ae14a5
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-10 22:10:14 +0400

    ms-transfer TransferMapper created

---
+++ ms-transfer/src/main/java/org/unitech/mstransfer/mapper/TransferMapper.java
@@ -0,0 +1,46 @@
+package org.unitech.mstransfer.mapper;
+
+import org.springframework.stereotype.Component;
+import org.unitech.mstransfer.domain.entity.Transfer;
+import org.unitech.mstransfer.model.dto.request.TransferRequest;
+import org.unitech.mstransfer.model.dto.response.TransferResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class TransferMapper {
+
+    public Transfer toEntity(TransferRequest request) {
+        Transfer transfer = new Transfer();
+        transfer.setFromAccountId(request.getFromAccountId());
+        transfer.setToAccountId(request.getToAccountId());
+        transfer.setAmount(request.getAmount());
+        transfer.setCurrencyFrom(request.getCurrencyFrom());
+        transfer.setCurrencyTo(request.getCurrencyTo());
+        transfer.setDescription(request.getDescription());
+        return transfer;
+    }
+
+    public TransferResponse toResponse(Transfer transfer) {
+        TransferResponse response = new TransferResponse();
+        response.setId(transfer.getId());
+        response.setFromAccountId(transfer.getFromAccountId());
+        response.setToAccountId(transfer.getToAccountId());
+        response.setAmount(transfer.getAmount());
+        response.setCurrencyFrom(transfer.getCurrencyFrom());
+        response.setCurrencyTo(transfer.getCurrencyTo());
+        response.setExchangeRate(transfer.getExchangeRate());
+        response.setConvertedAmount(transfer.getConvertedAmount());
+        response.setStatus(transfer.getStatus());
+        response.setDescription(transfer.getDescription());
+        response.setCreatedAt(transfer.getCreatedAt());
+        return response;
+    }
+
+    public List<TransferResponse> toResponseList(List<Transfer> transfers) {
+        return transfers.stream()
+                .map(this::toResponse)
+                .collect(Collectors.toList());
+    }
+}
