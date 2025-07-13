45effe7f04d06e84bc1617a9df67a7247ae7d3aa
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-13 20:35:15 +0400

    client updated

--- ms-bff/src/main/java/org/unitech/msbff/client/AccountClient.java
+++ ms-bff/src/main/java/org/unitech/msbff/client/AccountClient.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.unitech.msbff.dto.AccountResponse;
 
+import java.util.List;
+
 @FeignClient(name = "ms-account", path = "/accounts")
 public interface AccountClient {
+
+    @GetMapping("/user/{userId}")
+    List<AccountResponse> getUserAccounts(@PathVariable Long userId);
 
     @GetMapping("/{id}")
     AccountResponse getAccountById(@PathVariable Long id);
