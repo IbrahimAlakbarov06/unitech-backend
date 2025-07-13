a14c454de6d78835512fc7f0a486528436dd9bfa
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-13 20:34:06 +0400

    Client classes updated in ms-bff

--- ms-bff/src/main/java/org/unitech/msbff/client/TransferClient.java
+++ ms-bff/src/main/java/org/unitech/msbff/client/TransferClient.java
@@ -3,16 +3,20 @@ package org.unitech.msbff.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.unitech.msbff.dto.TransferResponse;
 
 import java.util.List;
 
-@FeignClient(name = "ms-transfer")
+@FeignClient(name = "ms-transfer", path = "/transfers")
 public interface TransferClient {
 
-    @GetMapping("/transfers/account/{accountId}")
-    List<TransferResponse> getTransfersByAccount(@PathVariable Long accountId);
+    @GetMapping("/user/{userId}/recent")
+    List<TransferResponse> getRecentTransfersByUser(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "10") int limit
+    );
 
-    @GetMapping("/transfers/{id}")
-    TransferResponse getTransferById(@PathVariable Long id);
+    @GetMapping("/account/{accountId}")
+    List<TransferResponse> getTransfersByAccount(@PathVariable Long accountId);
 }
