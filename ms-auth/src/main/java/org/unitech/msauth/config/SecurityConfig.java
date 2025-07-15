9a20874f7c833dfb143d9526010bb99d9967e144
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-07-15 23:29:09 +0400

    ms-auth adding new permit to SecurityConfig

--- ms-auth/src/main/java/org/unitech/msauth/config/SecurityConfig.java
+++ ms-auth/src/main/java/org/unitech/msauth/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .requestMatchers("/api/users/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
